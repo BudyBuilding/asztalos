debfc44577b4af353db7c52135719ee3c3db2efc
DESKTOP-J9JD4AJ\Ai <nyesteszilv@gmail.com>
2024-05-25 09:59:31 +0200

    Updating the object and their files too.

--- asztalos/backend/backend/src/main/java/asztalos/service/ObjectService.java
+++ asztalos/backend/backend/src/main/java/asztalos/service/ObjectService.java
@@ -1,34 +1,33 @@
 package asztalos.service;
 
-    import java.util.List;
-    import java.util.Optional;
+import java.util.List;
+import java.util.Optional;
 
-    import org.springframework.beans.factory.annotation.Autowired;
-    import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-    import asztalos.model.Object;
+import asztalos.model.WorkObject;
 import asztalos.repository.ObjectRepository;
 
-    @Service
-    public class ObjectService {
+@Service
+public class ObjectService {
 
-        @Autowired
-        private ObjectRepository objectRepository;
+    @Autowired
+    private ObjectRepository objectRepository;
 
-        public List<Object> findAll() {
-            return objectRepository.findAll();
-        }
-
-        public Optional<Object> findById(Long id) {
-            return objectRepository.findById(id);
-        }
-
-        public Object save(Object object) {
-            return objectRepository.save(object);
-        }
-
-        public void deleteById(Long id) {
-            objectRepository.deleteById(id);
-        }
-
+    public List<WorkObject> findAll() {
+        return objectRepository.findAll();
     }
+
+    public Optional<WorkObject> findById(Long id) {
+        return objectRepository.findById(id);
+    }
+
+    public WorkObject save(WorkObject workObject) {
+        return objectRepository.save(workObject);
+    }
+
+    public void deleteById(Long id) {
+        objectRepository.deleteById(id);
+    }
+}
