9c2cee1d4d7ecb6ed712275212ee8795470a18a0
DESKTOP-J9JD4AJAi <nyesteszilv@gmail.com>
2024-05-25 09:58:37 +0200

    Updating the user and their files too.

--- src/main/java/asztalos/controller/UserController.java
+++ src/main/java/asztalos/controller/UserController.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import asztalos.model.User;
@@ -26,33 +25,44 @@ public class UserController {
     private UserService userService;
 
     @GetMapping
-    public List<User> findAll() {
+    public List<User> getAllUsers() {
         return userService.findAll();
     }
 
     @GetMapping("/{id}")
-    public Optional<User> findById(@PathVariable Long id) {
-        return userService.findById(id);
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        Optional<User> user = userService.findById(id);
+        if (user.isPresent()) {
+            return ResponseEntity.ok(user.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
-    // create a User
-    @ResponseStatus(HttpStatus.CREATED) // 201
     @PostMapping
-    public User create(@RequestBody User User) {
-        return userService.save(User);
+    public User createUser(@RequestBody User user) {
+        return userService.save(user);
     }
 
-    // update a User
-    @PutMapping
-    public User update(@RequestBody User User) {
-        return userService.save(User);
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        Optional<User> user = userService.findById(id);
+        if (user.isPresent()) {
+            User updatedUser = user.get();
+            updatedUser.setName(userDetails.getName());
+            updatedUser.setAddress(userDetails.getAddress());
+            updatedUser.setTelephone(userDetails.getTelephone());
+            updatedUser.setUsername(userDetails.getUsername());
+            updatedUser.setSold(userDetails.getSold());
+            return ResponseEntity.ok(userService.save(updatedUser));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
-    // delete a User
-    @ResponseStatus(HttpStatus.NO_CONTENT) // 204
     @DeleteMapping("/{id}")
-    public void deleteById(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         userService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
-
 }
