dd599be05cadaa266313129135219ef15017b4c3
BudyBuilding <90955596+BudyBuilding@users.noreply.github.com>
2024-05-25 10:25:24 +0200

    Uploading the leftover

---
+++ asztalos/backend/backend/src/main/java/asztalos/repository/ScriptItemRepository.java
@@ -0,0 +1,10 @@
+package asztalos.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import asztalos.model.ScriptItem;
+
+@Repository
+public interface ScriptItemRepository extends JpaRepository<ScriptItem, Long> {
+}
