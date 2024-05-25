f31a49a9dbe650d12d35362b06e7e45b416100a5
DESKTOP-J9JD4AJAi <nyesteszilv@gmail.com>
2024-05-25 10:00:32 +0200

    Updating the color and their files too.

--- src/main/java/asztalos/service/ColorService.java
+++ src/main/java/asztalos/service/ColorService.java
@@ -1,34 +1,32 @@
 package asztalos.service;
 
-    import java.util.List;
-    import java.util.Optional;
+import java.util.List;
 
-    import org.springframework.beans.factory.annotation.Autowired;
-    import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-    import asztalos.model.Color;
+import asztalos.model.Color;
 import asztalos.repository.ColorRepository;
 
-    @Service
-    public class ColorService {
+@Service
+public class ColorService {
 
-        @Autowired
-        private ColorRepository colorRepository;
+    @Autowired
+    private ColorRepository colorRepository;
 
-        public List<Color> findAll() {
-            return colorRepository.findAll();
-        }
-
-        public Optional<Color> findById(Long id) {
-            return colorRepository.findById(id);
-        }
-
-        public Color save(Color color) {
-            return colorRepository.save(color);
-        }
-
-        public void deleteById(Long id) {
-            colorRepository.deleteById(id);
-        }
-
+    public Color saveColor(Color color) {
+        return colorRepository.save(color);
     }
+
+    public Color getColorById(Long id) {
+        return colorRepository.findById(id).orElse(null);
+    }
+
+    public List<Color> getAllColors() {
+        return colorRepository.findAll();
+    }
+
+    public void deleteColor(Long id) {
+        colorRepository.deleteById(id);
+    }
+}
