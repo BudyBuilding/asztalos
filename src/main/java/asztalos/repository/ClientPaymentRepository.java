a2626018472d1a4de14cee9f8ae5659c4bf43fff
DESKTOP-J9JD4AJAi <nyesteszilv@gmail.com>
2024-06-06 11:31:26 +0200

    Resolving the Client and Userpayment crud actions.

--- src/main/java/asztalos/repository/ClientPaymentRepository.java
+++ src/main/java/asztalos/repository/ClientPaymentRepository.java
@@ -1,10 +1,18 @@
 package asztalos.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import asztalos.model.Client;
 import asztalos.model.ClientPayment;
+import asztalos.model.User;
+import asztalos.model.Work;
 
 @Repository
 public interface ClientPaymentRepository extends JpaRepository<ClientPayment, Long> {
+    public List<ClientPayment> findByUser(User user);
+    public List<ClientPayment> findByClient(Client client);
+    public List<ClientPayment> findByWork(Work work);
 }
--- src/main/java/asztalos/service/ClientPaymentService.java
+++ src/main/java/asztalos/service/ClientPaymentService.java
@@ -1,12 +1,17 @@
 package asztalos.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import asztalos.model.Client;
 import asztalos.model.ClientPayment;
+import asztalos.model.User;
+import asztalos.model.Work;
 import asztalos.repository.ClientPaymentRepository;
+
 
 @Service
 public class ClientPaymentService {
@@ -14,19 +19,31 @@ public class ClientPaymentService {
     @Autowired
     private ClientPaymentRepository clientPaymentRepository;
 
-    public ClientPayment saveClientPayment(ClientPayment clientPayment) {
+    public ClientPayment save(ClientPayment clientPayment) {
         return clientPaymentRepository.save(clientPayment);
     }
 
-    public ClientPayment getClientPaymentById(Long id) {
-        return clientPaymentRepository.findById(id).orElse(null);
+    public Optional<ClientPayment> findById(Long id) {
+        return clientPaymentRepository.findById(id);
     }
 
-    public List<ClientPayment> getAllClientPayments() {
+    public List<ClientPayment> findByUser(User user) {
+        return clientPaymentRepository.findByUser(user);
+    }
+
+    public List<ClientPayment> findByClient(Client client) {
+        return clientPaymentRepository.findByClient(client);
+    }
+    
+    public List<ClientPayment> findByWork(Work work) {
+        return clientPaymentRepository.findByWork(work);
+    }
+
+    public List<ClientPayment> findAll() {
         return clientPaymentRepository.findAll();
     }
 
-    public void deleteClientPayment(Long id) {
+    public void delete(Long id) {
         clientPaymentRepository.deleteById(id);
     }
 }
