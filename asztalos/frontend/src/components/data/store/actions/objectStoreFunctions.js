ecdc39e5201f191e80393dd664599db85784258a
DESKTOP-J9JD4AJ\Ai <nyesteszilv@gmail.com>
2024-06-18 16:21:06 +0200

    Connecting the workobject class to the frontend.

--- asztalos/frontend/src/components/data/store/actions/objectStoreFunctions.js
+++ asztalos/frontend/src/components/data/store/actions/objectStoreFunctions.js
@@ -6,14 +6,29 @@ export const addObject = (object) => ({
   payload: object,
 });
 
-export const modifyObject = (modifiedObject) => ({
-  type: "MODIFY_OBJECT",
+export const addMoreObjects = (objects) => ({
+  type: "ADD_MORE_OBJECTs",
+  payload: objects,
+});
+
+export const updateObject = (modifiedObject) => ({
+  type: "UPDATE_OBJECT",
   payload: modifiedObject,
 });
 
-export const selectObject = (objectKey) => ({
+export const selectObject = (objectId) => ({
   type: "SELECT_OBJECT",
-  payload: objectKey,
+  payload: objectId,
+});
+
+export const addcreatedItem = (createdItem) => ({
+  type: "ADD_CREATED_ITEMS",
+  payload: createdItem,
+});
+
+export const addMoreCreatedItems = (createdItems) => ({
+  type: "ADD_MORE_CREATED_ITEMS",
+  payload: createdItems,
 });
 
 ///////////
@@ -21,27 +36,35 @@ export const selectObject = (objectKey) => ({
 const initialState = {
   objects: [],
   selectedObject: "0",
+  createdItems: [],
 };
 
 ///////////
 // Reducers
-const objectReducer = (state = [], action) => {
+export const objectReducer = (state = [], action) => {
   switch (action.type) {
     case "ADD_OBJECT":
-      if (!state.some((obj) => obj.key === action.payload.key)) {
+      if (!state.some((obj) => obj.objectId === action.payload.objectId)) {
         return [...state, action.payload];
       }
-
-    case "MODIFY_OBJECT":
+    case "ADD_MORE_OBJECTS":
+      const newObjects = action.payload.filter(
+        (object) =>
+          !state.some(
+            (existingObject) => existingObject.objectId === object.objectId
+          )
+      );
+      return [...state, ...newObjects];
+    case "UPDATE_OBJECT":
       return state.map((obj) =>
-        obj.key === action.payload.key ? action.payload : obj
+        obj.objectId === action.payload.objectId ? action.payload : obj
       );
     default:
       return state;
   }
 };
 
-const selectedObjectReducer = (state = "0", action) => {
+export const selectedObjectReducer = (state = "0", action) => {
   switch (action.type) {
     case "SELECT_OBJECT":
       return action.payload;
@@ -50,7 +73,28 @@ const selectedObjectReducer = (state = "0", action) => {
   }
 };
 
-export default { objectReducer, selectedObjectReducer };
+export const createdItemsReducer = (state = "0", action) => {
+  switch (action.type) {
+    case "ADD_CREATED_ITEM":
+      if (
+        !state.some(
+          (createdItem) => createdItem.itemId === action.payload.itemId
+        )
+      ) {
+        return [...state, action.payload];
+      }
+      return state;
+    case "ADD_MORE_CREATED_ITEMS":
+      return [
+        ...state,
+        ...action.payload.filter(
+          (createdItem) => !state.some((s) => s.itemId === createdItem.itemId)
+        ),
+      ];
+    default:
+      return state;
+  }
+};
 
 ///////////
 // Getters
--- asztalos/frontend/src/components/reusable/managers/storeManager.js
+++ asztalos/frontend/src/components/reusable/managers/storeManager.js
@@ -1,6 +1,8 @@
 //storeManager.js
 import clientApi from "../../data/api/clientApi";
+import objectApi from "../../data/api/objectApi";
 import scriptApi from "../../data/api/scriptApi";
+import scriptItemApi from "../../data/api/scriptItemApi";
 import tableApi from "../../data/api/tableApi";
 import workApi from "../../data/api/workApi";
 
@@ -20,8 +22,22 @@ export const fetchTables = (workId) => {
 };
 
 export const fetchScripts = () => {
-  console.log("fetching the tables");
+  console.log("fetching the scripts");
   scriptApi.getAllScriptsApi();
+};
+export const fetchScriptItemsForScript = (selectedScript) => {
+  console.log("fetching the scripts for: ", selectedScript);
+  scriptItemApi.getAllScriptItemsForScriptApi(selectedScript);
+};
+
+export const fetchObjects = () => {
+  console.log("fetching the objects");
+  scriptApi.getAllObjectsApi();
+};
+
+export const fetchObjectsForWork = (selectedWork) => {
+  console.log("fetching objects for the ", selectedWork);
+  objectApi.getObjectOfWorkApi(selectedWork);
 };
 
 /////////////
@@ -29,4 +45,5 @@ export const fetchAll = () => {
   console.log("fetching");
   fetchClients();
   fetchWorks();
+  fetchScripts();
 };
