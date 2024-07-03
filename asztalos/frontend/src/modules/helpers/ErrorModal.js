5884723e87a46003c355efa20b61696b7562a347
DESKTOP-J9JD4AJ\Ai <nyesteszilv@gmail.com>
2024-07-03 13:03:52 +0200

    Creating a function for printing out some errors if a Modal and autoclose after 5sec.

---
+++ asztalos/frontend/src/modules/helpers/ErrorModal.js
@@ -0,0 +1,27 @@
+// ErrorModal.js
+// showing some errors in modal
+
+import React, { useEffect } from "react";
+import { Modal } from "react-bootstrap";
+
+// the modal closes after 5sec
+const ErrorModal = ({ error, onClose }) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onClose();
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, [onClose]);
+
+  return (
+    <Modal show={true} onHide={onClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Error</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>{error}</Modal.Body>
+    </Modal>
+  );
+};
+
+export default ErrorModal;
