6ce8fba70675b8fa99dde017aa69d343bcd19394
DESKTOP-J9JD4AJ\Ai <nyesteszilv@gmail.com>
2024-07-15 10:08:40 +0200

    Making the uploading and downloading working for one file.

--- asztalos/backend/backend/src/main/java/asztalos/service/FilesService.java
+++ asztalos/backend/backend/src/main/java/asztalos/service/FilesService.java
@@ -1,8 +1,11 @@
+package asztalos.service;
 
-package  asztalos.service;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,10 +13,10 @@ import org.springframework.web.multipart.MultipartFile;
 import asztalos.model.Files;
 import asztalos.repository.FilesRepository;
 
-
-	
 @Service
 public class FilesService {
+
+    private static final Log logger = LogFactory.getLog(FilesService.class);
 
     @Autowired
     private FilesRepository fileRepository;
@@ -46,9 +49,17 @@ public class FilesService {
         return fileRepository.findByName(fileName).getImageData();
     }
 
-    public byte[] downloadFilesFromFileSystem(String fileName) throws IOException {
-        String path = fileRepository.findByName(fileName).getPath();
-
-        return java.nio.file.Files.readAllBytes(new File(path).toPath());
+public byte[] downloadFilesFromFileSystem(String fileName) throws IOException {
+    logger.info("Searching for file: " + fileName);
+    Files files = fileRepository.findByName(fileName);
+    if (files == null) {
+        logger.error("File not found in database: " + fileName);
+        throw new FileNotFoundException("File not found in database: " + fileName);
     }
+
+    String path = files.getPath();
+    logger.info("Path: " + path);
+    return java.nio.file.Files.readAllBytes(new File(path).toPath());
+}
+
 }
