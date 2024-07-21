7550da221aa2f6082da30440ae1cd2334141f356
DESKTOP-J9JD4AJ\Ai <nyesteszilv@gmail.com>
2024-07-21 21:34:42 +0200

    Adding some debugs.

--- asztalos/frontend/src/modules/components/SideNavigation.js
+++ asztalos/frontend/src/modules/components/SideNavigation.js
@@ -14,7 +14,8 @@ import { useNavigate } from "react-router-dom";
 function SideNavigation() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const userRole = getUser.role;
+  const userRole = dispatch(getUser()).role;
+
   const [showNav, setShowNav] = useState(true);
   const [showDropdown, setShowDropdown] = useState(false);
   const [showModal, setShowModal] = useState(false);
@@ -23,6 +24,7 @@ function SideNavigation() {
   const [isAdmin, setIsAdmin] = useState(false);
   const [user, setUser] = useState(null);
   const [selectedTab, setSelectedTab] = useState("dashboard");
+
   const handleSelectTab = (tab) => {
     if (tab !== selectedTab) {
       setSelectedTab(tab);
@@ -51,7 +53,10 @@ function SideNavigation() {
     fetchUser();
   }, [dispatch]);
 
-  const handleMouseEnter = () => setShowDropdown(true);
+  const handleMouseEnter = () => {
+    setShowNav(true);
+    setShowDropdown(true);
+  };
   const handleMouseLeave = () => setShowDropdown(false);
 
   const handleLogout = () => {
@@ -110,6 +115,12 @@ function SideNavigation() {
     handleSearch(e.target.value);
   };
 
+  const handleSearchClick = () => {
+    if (!showNav) {
+      setShowNav(true);
+    }
+  };
+
   return (
     <div
       className="d-flex flex-row position-relative"
@@ -122,259 +133,210 @@ function SideNavigation() {
       }}
     >
       <Nav className="flex-column pt-3 h-100 align-items-left">
-        {showNav ? (
-          <>
-            <Nav.Link>
+        <Nav.Link
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          className=" position-relative "
+          style={{ height: "3rem" }}
+        >
+          {showNav ? (
+            <Button variant="outline-primary">{user?.name || "Profile"}</Button>
+          ) : (
+            <i className={`bi bi-person-check`}></i>
+          )}
+          {showDropdown && (
+            <div
+              style={{
+                position: "absolute",
+                top: "110%",
+                left: 15,
+                borderRadius: "1rem",
+                zIndex: 10,
+                width: "200px",
+                cursor: "default",
+              }}
+            >
               <div
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
-                className="ms-2"
                 style={{
-                  position: "relative",
-                  display: "inline-block",
-                  height: "3rem",
+                  backgroundColor: "#fff",
+                  padding: "10px",
+                  marginBottom: "5px",
+                  border: "1px solid #ccc",
+                  borderRadius: "1rem",
+                  boxShadow: "0 2px 5px rgba(0, 0, 0, 0.15)",
                 }}
               >
-                <Button variant="outline-primary">
-                  {user?.name || "Profile"}
-                </Button>
-                {showDropdown && (
-                  <div
+                <strong>{user?.name}</strong>
+                <br />
+                {user?.email}
+              </div>
+              <Button
+                variant="light"
+                style={{
+                  textAlign: "left",
+                  width: "100%",
+                  padding: "10px",
+                  marginBottom: "5px",
+                  border: "1px solid #ccc",
+                  borderRadius: "1rem",
+                  boxShadow: "0 2px 5px rgba(0, 0, 0, 0.15)",
+                }}
+                onClick={() => handleSelectTab("settings")}
+              >
+                Settings
+              </Button>
+              <Button
+                variant="light"
+                style={{
+                  textAlign: "left",
+                  width: "100%",
+                  padding: "10px",
+                  marginBottom: "5px",
+                  border: "1px solid #ccc",
+                  borderRadius: "1rem",
+                  boxShadow: "0 2px 5px rgba(0, 0, 0, 0.15)",
+                }}
+                onClick={handleShowModal}
+              >
+                Logout
+              </Button>
+            </div>
+          )}
+        </Nav.Link>
+        <Nav.Link
+          className=" position-relative"
+          onClick={() => setShowNav(true)}
+          style={{
+            width: "100%",
+            height: "3rem",
+          }}
+        >
+          {!showNav ? (
+            <div className="justify-content-center">
+              <i className={`bi bi-search`}></i>
+            </div>
+          ) : (
+            <div className="d-flex align-items-center">
+              <Form className="d-flex w-100">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                  value={searchQuery}
+                  onChange={handleSearchChange}
+                  onClick={handleSearchClick} // Ensure nav opens on click
+                  style={{ width: "100%" }}
+                />
+              </Form>
+            </div>
+          )}
+          {searchResults.length > 0 && (
+            <Container className="position-absolute">
+              <Dropdown.Menu
+                show
+                className="mt-2"
+                style={{
+                  top: "0",
+                  left: "0",
+                  width: "90%",
+                  textAlign: "center",
+                }}
+              >
+                {searchResults.map((result, index) => (
+                  <Dropdown.Item
+                    key={index}
+                    href="#"
                     style={{
-                      position: "absolute",
-                      top: "100%",
-                      left: 0,
-                      backgroundColor: "#fff",
-                      border: "1px solid #ccc",
-                      zIndex: 1,
-                      width: "200px",
-                      boxShadow: "0 2px 5px rgba(0, 0, 0, 0.15)",
+                      overflow: "hidden",
+                      width: "100%",
                     }}
                   >
-                    <div
-                      style={{
-                        padding: "10px",
-                        borderBottom: "1px solid #ccc",
-                        backgroundColor: "#f8f9fa",
-                      }}
-                    >
-                      <strong>{user?.name}</strong>
-                      <br />
-                      {user?.email}
-                    </div>
-                    <Button
-                      variant="light"
+                    {result.name || result.title || result.email}{" "}
+                    <em
+                      key={index}
+                      href="#"
                       style={{
                         width: "100%",
-                        textAlign: "left",
-                        borderTop: "1px solid #ccc",
-                      }}
-                      onClick={() => handleSelectTab("settings")}
-                    >
-                      Settings
-                    </Button>
-                    <Button
-                      variant="light"
-                      style={{
-                        width: "100%",
-                        textAlign: "left",
-                        borderTop: "1px solid #ccc",
-                      }}
-                      onClick={handleShowModal}
-                    >
-                      Logout
-                    </Button>
-                  </div>
-                )}
-              </div>
-            </Nav.Link>
-            <Nav.Link className="p-0">
-              <Container
-                className="position-relative"
-                style={{
-                  margin: "auto",
-                  width: "100%",
-                  height: "3rem",
-                }}
-              >
-                <div className="d-flex align-items-center">
-                  <Form className="d-flex w-100">
-                    <Form.Control
-                      type="search"
-                      placeholder="Search"
-                      className="me-2"
-                      aria-label="Search"
-                      value={searchQuery}
-                      onChange={handleSearchChange}
-                      style={{ width: "100%" }}
-                    />
-                  </Form>
-                </div>
-                {searchResults.length > 0 && (
-                  <Container className="position-absolute">
-                    <Dropdown.Menu
-                      show
-                      className="mt-2"
-                      style={{
-                        top: "0",
-                        left: "0",
-                        width: "90%",
-                        textAlign: "center",
                       }}
                     >
-                      {searchResults.map((result, index) => (
-                        <Dropdown.Item
-                          key={index}
-                          href="#"
-                          style={{
-                            overflow: "hidden",
-                            width: "100%",
-                          }}
-                        >
-                          {result.name || result.title || result.email}{" "}
-                          <em
-                            key={index}
-                            href="#"
-                            style={{
-                              width: "100%",
-                            }}
-                          >
-                            ({result.type})
-                          </em>
-                        </Dropdown.Item>
-                      ))}
-                    </Dropdown.Menu>
-                  </Container>
-                )}
-              </Container>
-            </Nav.Link>
-            <Nav.Link
-              onClick={() => handleSelectTab("dashboard")}
-              style={navLinkStyle("dashboard")}
-            >
-              <i className="bi bi-house me-2"></i>
-              Dashboard
-            </Nav.Link>
-            <Nav.Link
-              onClick={() => handleSelectTab("users")}
-              style={navLinkStyle("users")}
-            >
+                      ({result.type})
+                    </em>
+                  </Dropdown.Item>
+                ))}
+              </Dropdown.Menu>
+            </Container>
+          )}
+        </Nav.Link>
+        <Nav.Link
+          onClick={() => handleSelectTab("dashboard")}
+          style={navLinkStyle("dashboard")}
+        >
+          {selectedTab == "dashboard" ? (
+            <i className="bi-house-fill  me-2"></i>
+          ) : (
+            <i className="bi bi-house me-2"></i>
+          )}
+          {showNav ? "Dashboard" : ""}
+        </Nav.Link>
+        {userRole == "admin" ? (
+          <Nav.Link
+            onClick={() => handleSelectTab("users")}
+            style={navLinkStyle("users")}
+          >
+            {selectedTab == "users" ? (
+              <i className="bi-person-fill  me-2"></i>
+            ) : (
               <i className="bi bi-person me-2"></i>
-              Users
-            </Nav.Link>
-            <Nav.Link
-              onClick={() => handleSelectTab("clients")}
-              style={navLinkStyle("clients")}
-            >
-              <i className="bi bi-people me-2"></i>
-              Clients
-            </Nav.Link>
-            <Nav.Link
-              onClick={() => handleSelectTab("works")}
-              style={navLinkStyle("works")}
-            >
-              <i className="bi bi-archive me-2"></i>
-              Works
-            </Nav.Link>
-            <Nav.Link
-              onClick={() => handleSelectTab("scripts")}
-              style={navLinkStyle("scripts")}
-            >
-              <i className="bi bi-file-earmark-code me-2"></i>
-              Scripts
-            </Nav.Link>
-            <Nav.Link
-              onClick={() => handleSelectTab("colors")}
-              style={navLinkStyle("colors")}
-            >
-              <i className="bi bi-palette me-2"></i>
-              Colors
-            </Nav.Link>
-          </>
-        ) : (
-          <>
-            <Nav.Link
-              className="d-flex justify-content-center align-items-center"
-              style={{ width: "100%", height: "3rem" }}
-            >
-              <i className={`bi bi-person-check`}></i>
-            </Nav.Link>{" "}
-            <Nav.Link
-              className="d-flex justify-content-center align-items-center"
-              style={{ width: "100%", height: "3rem" }}
-            >
-              <i className={`bi bi-search`}></i>
-            </Nav.Link>{" "}
-            <Nav.Link
-              onClick={() => handleSelectTab("dashboard")}
-              className="d-flex justify-content-center align-items-center"
-              style={{ width: "100%", height: "3rem" }}
-            >
-              <i
-                className={`bi ${
-                  selectedTab === "dashboard" ? "bi-house-fill" : "bi-house"
-                }`}
-              ></i>
-            </Nav.Link>
-            <Nav.Link
-              onClick={() => handleSelectTab("users")}
-              className="d-flex justify-content-center align-items-center"
-              style={{ width: "100%", height: "3rem" }}
-            >
-              <i
-                className={`bi ${
-                  selectedTab === "users" ? "bi-person-fill" : "bi-person"
-                }`}
-              ></i>
-            </Nav.Link>
-            <Nav.Link
-              onClick={() => handleSelectTab("clients")}
-              className="d-flex justify-content-center align-items-center"
-              style={{ width: "100%", height: "3rem" }}
-            >
-              <i
-                className={`bi ${
-                  selectedTab === "clients" ? "bi-people-fill" : "bi-people"
-                }`}
-              ></i>
-            </Nav.Link>
-            <Nav.Link
-              onClick={() => handleSelectTab("works")}
-              className="d-flex justify-content-center align-items-center"
-              style={{ width: "100%", height: "3rem" }}
-            >
-              <i
-                className={`bi ${
-                  selectedTab === "works" ? "bi-archive-fill" : "bi-archive"
-                }`}
-              ></i>
-            </Nav.Link>
-            <Nav.Link
-              onClick={() => handleSelectTab("scripts")}
-              className="d-flex justify-content-center align-items-center"
-              style={{ width: "100%", height: "3rem" }}
-            >
-              <i
-                className={`bi ${
-                  selectedTab === "scripts"
-                    ? "bi-file-earmark-code-fill"
-                    : "bi-file-earmark-code"
-                }`}
-              ></i>
-            </Nav.Link>
-            <Nav.Link
-              onClick={() => handleSelectTab("colors")}
-              className="d-flex justify-content-center align-items-center"
-              style={{ width: "100%", height: "3rem" }}
-            >
-              <i
-                className={`bi ${
-                  selectedTab === "colors" ? "bi-palette-fill" : "bi-palette"
-                }`}
-              ></i>
-            </Nav.Link>
-          </>
-        )}
+            )}
+            {showNav ? "users" : ""}
+          </Nav.Link>
+        ) : null}
+        <Nav.Link
+          onClick={() => handleSelectTab("clients")}
+          style={navLinkStyle("clients")}
+        >
+          {selectedTab == "clients" ? (
+            <i className="bi bi-people-fill me-2"></i>
+          ) : (
+            <i className="bi bi-people me-2"></i>
+          )}
+
+          {showNav ? "Clients" : ""}
+        </Nav.Link>
+        <Nav.Link
+          onClick={() => handleSelectTab("works")}
+          style={navLinkStyle("works")}
+        >
+          {selectedTab == "works" ? (
+            <i className="bi bi-archive-fill me-2"></i>
+          ) : (
+            <i className="bi bi-archive me-2"></i>
+          )}
+          {showNav ? "Works" : ""}
+        </Nav.Link>
+        <Nav.Link
+          onClick={() => handleSelectTab("scripts")}
+          style={navLinkStyle("scripts")}
+        >
+          {selectedTab == "scripts" ? (
+            <i className="bi bi-file-earmark-code-fill me-2"></i>
+          ) : (
+            <i className="bi bi-file-earmark-code me-2"></i>
+          )}
+          {showNav ? "Scripts" : ""}
+        </Nav.Link>
+        <Nav.Link
+          onClick={() => handleSelectTab("colors")}
+          style={navLinkStyle("colors")}
+        >
+          {selectedTab == "colors" ? (
+            <i className="bi bi-palette-fill me-2"></i>
+          ) : (
+            <i className="bi bi-palette me-2"></i>
+          )}
+          {showNav ? "Colors" : ""}
+        </Nav.Link>
       </Nav>
       <Button
         onClick={toggleNav}
