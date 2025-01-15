fb7713c79717593e4dec7e428c0734e3dffa3bc3
Budy <nyesteszilv@gmail.com>
2025-01-15 17:14:08 +0100

    Adding logic to navigatie from WorksPage to WorkAnalyzer

--- asztalos/frontend/src/modules/components/WorksPage.js
+++ asztalos/frontend/src/modules/components/WorksPage.js
@@ -3,17 +3,32 @@ import React, { useState, useEffect } from "react";
 import { Table } from "react-bootstrap";
 import { getAllWorks } from "../../data/getters";
 import Loading from "../helpers/Loading";
+import { useDispatch } from "react-redux"; // Redux hook
+import { selectWork } from "../../data/store/actions/workStoreFunctions"; // Action
+import { useNavigate } from "react-router-dom"; // Router hook
+
 function WorksPage() {
   const [works, setWorks] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [render, setRender] = useState(true);
+  
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   useEffect(() => {
-    function loadWorks() {
+    async function loadWorks() {
+      const data = await getAllWorks();
+      setWorks(data);
       setLoading(false);
-      return getAllWorks();
     }
-    setWorks(loadWorks());
-  }, [render]);
+    loadWorks();
+  }, []);
+
+  // Sor kattintás eseménykezelő
+  const handleRowClick = (work) => {
+    dispatch(selectWork(work)); // Beállítjuk a kiválasztott munkát a Redux store-ba
+    navigate(`/workAnalyzer/${work.workId}`); // Átirányítás a workAnalyzer oldalra
+  };
+
   return (
     <div>
       <h1>Works Page</h1>
@@ -26,15 +41,21 @@ function WorksPage() {
               <tr>
                 <th>ID</th>
                 <th>Title</th>
-                <th>Description</th>
+                <th>Status</th>
               </tr>
             </thead>
             <tbody>
               {works.map((work) => (
-                <tr key={work.workId}>
+                <tr
+                  key={work.workId}
+                  onClick={() => handleRowClick(work)} // Sor kattintás
+                  style={{
+                    cursor: "pointer",
+                  }}
+                >
                   <td>{work.workId}</td>
-                  <td>{work.title}</td>
-                  <td>{work.description}</td>
+                  <td>{work.name}</td>
+                  <td>{work.status}</td>
                 </tr>
               ))}
             </tbody>
