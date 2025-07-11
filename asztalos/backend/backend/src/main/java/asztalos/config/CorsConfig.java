ea23dca4d5c55bb4243512adfdca4d65a36f582e
Budy <nyesteszilv@gmail.com>
2025-07-11 15:37:06 +0200

    cors modif

--- asztalos/backend/backend/src/main/java/asztalos/config/CorsConfig.java
+++ asztalos/backend/backend/src/main/java/asztalos/config/CorsConfig.java
@@ -11,6 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")  // Dinamikusan megadott origin
+                .allowedHeaders("*")  
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true)
                 .maxAge(3600);
